cb9217fadd55fb18b9528f07463d6d9d51b07c61
Eli Hartnett <55256366+Elichartnett@users.noreply.github.com>
2021-11-07 08:38:37 -0600

    Initial Commit

---
+++ Calculator App Challenge/Calculator_App_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_App_ChallengeApp.swift
+//  Calculator App Challenge
+//
+//  Created by Eli Hartnett on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_App_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator App Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator App Challenge
+//
+//  Created by Eli Hartnett on 11/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
